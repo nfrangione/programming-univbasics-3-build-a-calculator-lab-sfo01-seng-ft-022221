bad86a4f0e366568e2a49b32cbd8e23046bcf5c1
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-10 00:51:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,20 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
+  quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  remainder
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  root
 end
